69f67640a7d9c9e75d31959ae15d1feaffd404cb
Bryan Lenihan <bryan@rolesvillesoftware.com>
2021-01-23 09:53:25 -0500

    Http Configuration

---
+++ CanvasApi.Client/CanvasApiClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("CanvasApi.Client.Test")]
+namespace CanvasApi.Client
+{
+    public class CanvasApiClient
+    {
+        private HttpClient Client { get; set; }
+
+        public CanvasApiClient(string canvasDomain)
+        {
+
+        }
+
+    }
+}
---
+++ CanvasApi.Client/HttpClientConfigurator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+
+namespace CanvasApi.Client
+{
+    public static class HttpClientConfigurator
+    {
+        public static HttpClient ConfigureCanvasApi(this HttpClient httpClient, string canvasDomain, string apiKey)
+        {
+            if (string.IsNullOrWhiteSpace(canvasDomain)) throw new ArgumentNullException(nameof(canvasDomain), "Canvas domain is a required element.");
+            if (string.IsNullOrWhiteSpace(apiKey)) throw new ArgumentNullException(nameof(apiKey), "API key is a required element.");
+
+            httpClient.BaseAddress = new Uri(canvasDomain, UriKind.Absolute);
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+
+            return httpClient;
+        }
+    }
+}

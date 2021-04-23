857eb64f080ffedbbca5f721861cd8a74c377e88
dbagnoli <dbagnoli9699@gmail.com>
2021-04-23 09:42:41 -0400

    Made changes to Module so the program would compile for PR

--- CanvasApi.Client/Modules/Models/Module.cs
+++ CanvasApi.Client/Modules/Models/Module.cs
@@ -8,14 +8,14 @@ namespace CanvasApi.Client.Modules.Models
     internal class Module
     {
         [JsonProperty("id")] public long Id { get; set; }
-        [JsonProperty("wokflow_state")] public IEnumerable<ModuleWorkflowState> WorkflowState { get; set; }
+        //[JsonProperty("wokflow_state")] public IEnumerable<ModuleWorkflowState> WorkflowState { get; set; }
         [JsonProperty("position")] public int Position { get; set; }
         [JsonProperty("name")] public string Name { get; set; }
         [JsonProperty("require_sequential_progression")] public bool RequireSequentialProgression { get; set; }
         [JsonProperty("prerequisite_module_ids")] public int[] PrerequisiteModuleIds { get; set; }
         [JsonProperty("items_count")] public int ItemsCount { get; set; }
         [JsonProperty("items_url")] public string ItemsUrl { get; set; }
-        [JsonProperty("items")] public IModuleItem[] Items { get; set; }
+        //[JsonProperty("items")] public IModuleItem[] Items { get; set; }
         [JsonProperty("publish_final_grade")] public bool PublishFinalGrade { get; set; }
     }
 }
